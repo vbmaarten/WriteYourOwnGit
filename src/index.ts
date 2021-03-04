100da7f4c1bb59beb3baa3d841f938b14b47942d
Maarten van Beek <maartenvanbeek@gmail.com>
2021-03-04 22:00:23 +0100

    First setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,66 @@
-const greeting = (name: string): string => {
-  return `hello ${name}! How are you?`;
+import yargs from "yargs";
+import path from "path";
+import fs from "fs";
+import ini from "ini";
+
+yargs.scriptName("wyag")
+  .command("init", false, () => { }, init)
+  .help()
+  .argv;
+
+function repo_path(repo: GitRepository, git_path: string){
+  return path.join(repo.gitdir, git_path);
+}
+
+function repo_dir(repo: GitRepository, git_path: string, mkdir: boolean = false): string{
+  const dir = repo_path(repo, git_path);
+
+  const dirStat = fs.statSync(dir);
+
+  if(dirStat.isFile()){
+    throw new Error(`Not a directory ${dir}`);
+  }
+  
+  if(!dirStat.isDirectory()){
+    if(!mkdir){
+      throw new Error(`Not a directory ${dir}`);
+    }
+    
+    fs.mkdirSync(dir);
+  }
+
+  return dir;
+}
+
+function repo_file(repo: GitRepository, git_path: string, mkdir: boolean = false){
+  const split_path = git_path.split(path.sep);
+  split_path.pop();
+
+  return repo_dir(repo, split_path.join(path.sep), mkdir);
 };
 
-console.log(greeting("Joe"));
+function repo_create(path: string){
+  const repo = new GitRepository(path);
+}
+
+class GitRepository {
+  public worktree: string;
+  public gitdir: string;
+  conf: {[key: string]: any}
+  
+  constructor(worktree_path: string, force: boolean = false){
+    this.worktree = worktree_path;
+    this.gitdir = path.join(this.worktree, '.git');
+
+    if(force || !fs.statSync(this.gitdir).isDirectory()){
+      throw new Error(`Directory ${this.gitdir} does not exist`);
+    }
+
+    const configFile = repo_file(this, "config");
+    this.conf = ini.parse(fs.readFileSync(configFile).toString('utf-8'));
+  }
+}
+
+function init() {
+  console.log('test');
+}
